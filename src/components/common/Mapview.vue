68a5c36b6c0434073beab31098f25d1d1ed1330f
lz <37628265@qq.com>
2023-04-04 18:09:41 +0800

    完成了地图初始化

---
+++ src/components/common/Mapview.vue
@@ -0,0 +1,42 @@
+<template>
+    <div id="mapView"></div>
+</template>
+
+<script>
+import { loadModules } from 'esri-loader'
+const option = {
+  url: 'https://js.arcgis.com/4.26/init.js',
+  css: 'https://js.arcgis.com/4.26/esri/themes/light/main.css'
+}
+export default {
+  //   name: 'Mapview',
+  components: {},
+  mounted: function () {
+    this.initMapView()
+  },
+  methods: {
+    async initMapView () {
+      const [Map, MapView] = await loadModules(['esri/Map', 'esri/views/MapView'], option)
+      const map = new Map({
+        basemap: 'osm'
+      })
+      const view = new MapView({
+        container: 'mapView',
+        map: map,
+        zoom: 10,
+        center: [114.426435, 23.122741]
+      })
+      view.ui.components = [] // 取消默认组件
+      console.log(view)
+    }
+  }
+}
+</script>
+
+<style scope>
+#mapView {
+    position: relative;
+    width: 100%;
+    height: 100%;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,48 @@
 <template>
-  <HellowMap></HellowMap>
+    <el-container class="home-container">
+        <el-header>一张图系统</el-header>
+        <el-container>
+            <el-aside width="200px">侧边栏区域</el-aside>
+            <el-main>
+                <MapviewVue></MapviewVue>
+            </el-main>
+        </el-container>
+    </el-container>
 </template>
 
 <script>
-import HellowMap from '@/components/HellowMap.vue'
+import MapviewVue from '@/components/common/Mapview.vue'
 export default {
   name: 'HomePage',
   components: {
-    HellowMap
+    MapviewVue
   }
 }
 </script>
 
-<style></style>
+<style>
+.home-container {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+}
+.el-header {
+    height: 70px !important;
+    background-color: pink;
+    display: flex;
+    justify-content: space-between;
+    padding-left: 0;
+    align-items: center;
+    padding-bottom: 7px;
+}
+.el-aside {
+    background-color:green;
+}
+
+.el-main {
+    background-color: #eaedf1;
+    padding: 0px !important;
+}
+</style>
